75cc6ed3574a7eb615a4dc51c5d0ac7ac4a8b318
Amol Deshmukh <34318357+des137@users.noreply.github.com>
2021-11-25 07:40:51 -0500

    Create parameter_shift.py

---
+++ parameter_shift.py
@@ -0,0 +1,31 @@
+import numpy as np
+from utils import bra_ket
+
+def parameter_shift(theta, H, ansatz_):
+    theta = np.array(theta)
+    len_th = len(theta)
+    
+    g = []
+    for i in range(len(theta)):
+        diff_ = np.zeros(len(theta))
+        diff_[i] = np.pi/2
+        vec_ = (ansatz_(theta + diff) - ansatz_(theta - diff))/2    
+        g.append(vec_)
+    g = np.array(g)
+    
+    M = np.zeros([len_th, len_th])
+    for i in range(len_th):
+        for j in range(len_th):
+            M[i, j] = np.real(
+                bra_ket(g[i], g[j])\
+              - bra_ket(g[i], ansatz_(theta) @ conj_tp(ansatz_(theta)) @ g[j])
+            )  
+
+    state = H @ ansatz_(theta)
+    C = []
+    for i in range(len(theta)):
+        C.append(g[i], state)
+    C = np.array(C)   
+
+    MC = np.linalg.pinv(M) @ C    
+    return np.real(MC)
